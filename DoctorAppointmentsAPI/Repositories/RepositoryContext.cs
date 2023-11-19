68ba7005cc640f942d4233d66ea875d1322754f3
Emre Savaş <emre_savas28@hotmail.com>
2023-11-19 15:45:08 +0300

    Add RepositoryContext and add EntityFrameworkCore package.

---
+++ DoctorAppointmentsAPI/Repositories/RepositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using DoctorAppointmentsDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorAppointmentsAPI.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions context) : base (context)
+        {
+
+        }
+
+        public DbSet<Doctors> Doctors { get; set; }
+        public DbSet<Patients> Patients { get; set; }
+        public DbSet<FamilyDoctors> FamilyDoctors { get; set; }
+        public DbSet<Appointments> Appointments { get; set; }
+        public DbSet<DoctorSpecialties> DoctorSpecialties { get; set; }
+        public DbSet<FamilyDoctorChanges> FamilyDoctorChanges { get; set; }
+        public DbSet<AppointmentMedications> AppointmentsMedications { get; set; }
+    }
+}
