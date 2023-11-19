b57727ccd72e5c4352b86c90b0f8019272624f6c
Emre Savaş <emre_savas28@hotmail.com>
2023-11-19 21:08:42 +0300

    Add AppointmentMedicationRepository.

---
+++ DoctorAppointmentsAPI/Repositories/AppointmentMedicationRepository.cs
@@ -0,0 +1,34 @@
+﻿using DoctorAppointmentsAPI.Repositories.Contracts;
+using DoctorAppointmentsDomain.Entities;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentsAPI.Repositories
+{
+    public class AppointmentMedicationRepository : RepositoryBase<AppointmentMedications>, IAppointmentMedicationRepository
+    {
+        public AppointmentMedicationRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public async Task CreateAppointmentMedication(AppointmentMedications appointmentMedication)
+            => await CreateAsync(appointmentMedication);;
+
+        public async Task DeleteAppointmentMedication(AppointmentMedications appointmentMedication)
+            => await DeleteAsync(appointmentMedication);
+
+        public async Task<IEnumerable<AppointmentMedications>> GetAllAppointmentMedicationsAsync(bool trackChanges)
+            => await FindAllAsync(trackChanges);
+
+        public async Task<AppointmentMedications> GetAppointmentMedicationByIdAsync(int medicationId, bool trackChanges)
+        {
+            var appointmentMedications = await FindByConditionAsync(a => a.MedicationId.Equals(medicationId), trackChanges);
+            return appointmentMedications.FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<AppointmentMedications>> GetAppointmentMedicationsByConditionAsync(Expression<Func<AppointmentMedications, bool>> expression, bool trackChanges)
+            => await FindByConditionAsync(expression, trackChanges);
+
+        public async Task UpdateAppointmentMedication(AppointmentMedications appointmentMedication)
+            => await UpdateAsync(appointmentMedication);
+    }
+}
---
+++ DoctorAppointmentsAPI/Repositories/Contracts/IAppointmentMedicationRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoctorAppointmentsDomain.Entities;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentsAPI.Repositories.Contracts
+{
+    public interface IAppointmentMedicationRepository : IRepositoryBase<AppointmentMedications>
+    {
+        Task<IEnumerable<AppointmentMedications>> GetAllAppointmentMedicationsAsync(bool trackChanges);
+        Task<AppointmentMedications> GetAppointmentMedicationByIdAsync(int medicationId, bool trackChanges);
+        Task<IEnumerable<AppointmentMedications>> GetAppointmentMedicationsByConditionAsync(Expression<Func<AppointmentMedications, bool>> expression, bool trackChanges);
+        Task CreateAppointmentMedication(AppointmentMedications appointmentMedication);
+        Task DeleteAppointmentMedication(AppointmentMedications appointmentMedication);
+        Task UpdateAppointmentMedication(AppointmentMedications appointmentMedication);
+    }
+}
