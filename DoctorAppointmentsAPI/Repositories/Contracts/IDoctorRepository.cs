2eaab5b50e7faec3fb4cd078440f2acc777de5e7
Emre Savaş <emre_savas28@hotmail.com>
2023-11-19 20:57:13 +0300

    Add DoctorRepository.

---
+++ DoctorAppointmentsAPI/Repositories/Contracts/IDoctorRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoctorAppointmentsDomain.Entities;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentsAPI.Repositories.Contracts
+{
+    public interface IDoctorRepository : IRepositoryBase<Doctors>
+    {
+        Task<IEnumerable<Doctors>> GetAllDoctorsAsync(bool trackChanges);
+        Task<Doctors> GetDoctorByIdAsync(int doctorId, bool trackChanges);
+        Task<IEnumerable<Doctors>> GetDoctorsByConditionAsync(Expression<Func<Doctors, bool>> expression, bool trackChanges);
+        Task CreateDoctor(Doctors doctor);
+        Task DeleteDoctor(Doctors doctor);
+        Task UpdateDoctor(Doctors doctor);
+    }
+}
---
+++ DoctorAppointmentsAPI/Repositories/DoctorRepository.cs
@@ -0,0 +1,36 @@
+﻿using DoctorAppointmentsAPI.Repositories.Contracts;
+using DoctorAppointmentsDomain.Entities;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentsAPI.Repositories
+{
+    public class DoctorRepository : RepositoryBase<Doctors>, IDoctorRepository
+    {
+        public DoctorRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public async Task CreateDoctor(Doctors doctor)
+            => await CreateAsync(doctor);
+
+        public async Task DeleteDoctor(Doctors doctor)
+            => await DeleteAsync(doctor);
+
+        public async Task<IEnumerable<Doctors>> GetAllDoctorsAsync(bool trackChanges)
+            => await FindAllAsync(trackChanges);
+
+        public async Task<Doctors> GetDoctorByIdAsync(int doctorId, bool trackChanges)
+        {
+            var doctors = await FindByConditionAsync(d => d.DoctorId.Equals(doctorId), trackChanges);
+            return doctors.FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<Doctors>> GetDoctorsByConditionAsync(Expression<Func<Doctors, bool>> expression, bool trackChanges)
+            => await FindByConditionAsync(expression, trackChanges);
+
+        public Task UpdateDoctor(Doctors doctor)
+            => UpdateAsync(doctor);
+    }
+    {
+    }
+}
