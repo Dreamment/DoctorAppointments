501d6d28bb4fd8b07c741b807559bca441624560
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 13:26:11 +0300

    Configure SQL DB connection.

--- DoctorAppointmentsAPI/Program.cs
+++ DoctorAppointmentsAPI/Program.cs
@@ -1,5 +1,6 @@
 using DoctorAppointmentsAPI.Repositories;
 using DoctorAppointmentsAPI.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RepositoryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 
 var app = builder.Build();
