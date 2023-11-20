417069eb9b8deba90189348ae2a03846459498eb
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 13:17:29 +0300

    Add RepositoryManager.

---
+++ DoctorAppointmentsAPI/Repositories/Contracts/IRepositoryManager.cs
@@ -0,0 +1,14 @@
+﻿namespace DoctorAppointmentsAPI.Repositories.Contracts
+{
+    public interface IRepositoryManager
+    {
+        IAppointmentMedicationRepository AppointmentMedication { get; }
+        IAppointmentRepository Appointment { get; }
+        IDoctorRepository Doctor { get; }
+        IDoctorSpecialityRepository DoctorSpeciality { get; }
+        IFamilyDoctorChangesRepository FamilyDoctorChanges { get; }
+        IFamilyDoctorRepository FamilyDoctor { get; }
+        IPatientRepository Patient { get; }
+        Task SaveAsync();
+    }
+}
