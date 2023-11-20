4d97dac1fab5d47ae79a11c9ccfc40874e753789
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 19:32:27 +0300

    Add IPatientService.

---
+++ DoctorAppointmentsAPI/Services/Contracts/IPatientService.cs
@@ -0,0 +1,12 @@
+﻿using DoctorAppointmentsAPI.DataTransferObjects;
+
+namespace DoctorAppointmentsAPI.Services.Contracts
+{
+    public interface IPatientService
+    {
+        Task<GetAppointmentsForPatientDto> GetAppointmentsForPatientAsync(int patientId, bool trackChanges);
+        Task <string>CreateAppointmentAsync(int patientId, CreateAppointmentDto appointmentDto, bool trackChanges);
+        Task<GetMedicationsForAppointmentDto> GetMedicationsForAppointmentAsync(int patientId, string appointmentCode, bool trackChanges);
+        Task<GetFamilyDoctorDto> GetFamilyDoctorAsync(int patientId, bool trackChanges);
+    }
+}
