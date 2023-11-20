78025c1e1d7f03e2a0d6def6deb7a3b2a013d6f8
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 17:40:39 +0300

    Add IDoctorService.

---
+++ DoctorAppointmentsAPI/Services/Contracts/IDoctorService.cs
@@ -0,0 +1,14 @@
+﻿using DoctorAppointmentsAPI.DataTransferObjects;
+using DoctorAppointmentsDomain.Entities;
+
+namespace DoctorAppointmentsAPI.Services.Contracts
+{
+    public interface IDoctorService
+    {
+        Task<IEnumerable<GetPatientsForFamilyDoctorDto>> GetPatientsAsync(int doctorId, bool trackChanges); // get all patients for the doctor
+        Task<IEnumerable<GetAppointmentsForDoctorDto>> GetTodaysAppointmentsAsync(int doctorId, bool trackChanges);
+        Task<IEnumerable<GetAppointmentsForDoctorDto>> GetUpcomingAppointmentsAsync(int doctorId, bool trackChanges);
+        Task<int> CreateMedicationAsync(int doctorId, CreateMedicationDto medicationDto, bool trackChanges);
+        Task ChangeAppointmentStatus(int doctorId, string appointmentCode, bool isApproved, bool trackChanges);
+    }
+}
