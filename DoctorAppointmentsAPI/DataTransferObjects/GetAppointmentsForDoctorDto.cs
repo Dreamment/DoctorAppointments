2956de47dfd52eb105a921830c79513f4c523bfa
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 17:41:08 +0300

    Add Dtos for GetTodaysAppointmentsAsync function.

---
+++ DoctorAppointmentsAPI/DataTransferObjects/GetAppointmentsForDoctorDto.cs
@@ -0,0 +1,12 @@
+﻿using DoctorAppointmentsDomain.Entities;
+
+namespace DoctorAppointmentsAPI.DataTransferObjects
+{
+    public class GetAppointmentsForDoctorDto
+    {
+        public GetPatientsForFamilyDoctorDto Patient { get; set; }
+        public DateTime AppointmentDateTime { get; set; }
+        public bool? Status { get; set; }
+        public IEnumerable<GetMedicationsForAppointmentDto>? Medications { get; set; }
+    }
+}
---
+++ DoctorAppointmentsAPI/DataTransferObjects/GetMedicationsForAppointmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DoctorAppointmentsAPI.DataTransferObjects
+{
+    public class GetMedicationsForAppointmentDto
+    {
+        public string Id { get; set; }
+        public string AppointmentCode { get; set; }
+        public string MedicationName { get; set; }
+        public string Dosage { get; set; }
+        public string UsageInstructions { get; set; }
+    }
+}
