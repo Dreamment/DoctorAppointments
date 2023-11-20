ff793f7efa52f1490785b3325f2a73b96ff82ad7
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 13:02:43 +0300

    Add FamilyDoctorChangesRepository.

---
+++ DoctorAppointmentsAPI/Repositories/Contracts/IFamilyDoctorChangesRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoctorAppointmentsDomain.Entities;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentsAPI.Repositories.Contracts
+{
+    public interface IFamilyDoctorChangesRepository : IRepositoryBase<FamilyDoctorChanges>
+    {
+        Task CreateFamilyDoctorChangeAsync(FamilyDoctorChanges familyDoctorChange);
+        Task DeleteFamilyDoctorChangeAsync(FamilyDoctorChanges familyDoctorChange);
+        Task<IEnumerable<FamilyDoctorChanges>> GetAllFamilyDoctorChangesAsync(bool trackChanges);
+        Task<FamilyDoctorChanges> GetFamilyDoctorChangeByIdAsync(int familyDoctorChangeId, bool trackChanges);
+        Task<IEnumerable<FamilyDoctorChanges>> GetFamilyDoctorChangesByConditionAsync(Expression<Func<FamilyDoctorChanges, bool>> expression, bool trackChanges);
+        Task UpdateFamilyDoctorChangeAsync(FamilyDoctorChanges familyDoctorChange);
+    }
+}
