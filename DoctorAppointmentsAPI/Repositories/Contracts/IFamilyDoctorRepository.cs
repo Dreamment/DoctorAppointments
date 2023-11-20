05a36d73cc79e0c4dbed61c5c1dd40d590699e83
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 12:57:28 +0300

    Add FamilyDoctorRepository.

---
+++ DoctorAppointmentsAPI/Repositories/Contracts/IFamilyDoctorRepository.cs
@@ -0,0 +1,15 @@
+﻿using DoctorAppointmentsDomain.Entities;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentsAPI.Repositories.Contracts
+{
+    public interface IFamilyDoctorRepository : IRepositoryBase<FamilyDoctors>
+    {
+        Task CreateFamilyDoctor(FamilyDoctors familyDoctor);
+        Task DeleteFamilyDoctor(FamilyDoctors familyDoctor);
+        Task<IEnumerable<FamilyDoctors>> GetAllFamilyDoctorsAsync(bool trackChanges);
+        Task<FamilyDoctors> GetFamilyDoctorByIdAsync(int familyDoctorId, bool trackChanges);
+        Task<IEnumerable<FamilyDoctors>> GetFamilyDoctorsByConditionAsync(Expression<Func<FamilyDoctors, bool>> expression, bool trackChanges);
+        Task UpdateFamilyDoctor(FamilyDoctors familyDoctor);
+    }
+}
---
+++ DoctorAppointmentsAPI/Repositories/FamilyDoctorRepository.cs
@@ -0,0 +1,34 @@
+﻿using DoctorAppointmentsAPI.Repositories.Contracts;
+using DoctorAppointmentsDomain.Entities;
+using System.Linq.Expressions;
+
+namespace DoctorAppointmentsAPI.Repositories
+{
+    public class FamilyDoctorRepository : RepositoryBase<FamilyDoctors>, IFamilyDoctorRepository
+    {
+        public FamilyDoctorRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public async Task CreateFamilyDoctor(FamilyDoctors familyDoctor)
+            => await CreateAsync(familyDoctor);
+
+        public async Task DeleteFamilyDoctor(FamilyDoctors familyDoctor)
+            => await DeleteAsync(familyDoctor);
+
+        public async Task<IEnumerable<FamilyDoctors>> GetAllFamilyDoctorsAsync(bool trackChanges)
+            => await FindAllAsync(trackChanges);
+
+        public async Task<FamilyDoctors> GetFamilyDoctorByIdAsync(int familyDoctorId, bool trackChanges)
+        {
+            var familyDoctors = await FindByConditionAsync(d => d.FamilyDoctorId.Equals(familyDoctorId), trackChanges);
+            return familyDoctors.FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<FamilyDoctors>> GetFamilyDoctorsByConditionAsync(Expression<Func<FamilyDoctors, bool>> expression, bool trackChanges)
+            => await FindByConditionAsync(expression, trackChanges);
+
+        public Task UpdateFamilyDoctor(FamilyDoctors familyDoctor)
+            => UpdateAsync(familyDoctor);
+    }
+}
