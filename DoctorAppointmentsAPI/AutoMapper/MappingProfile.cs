bfb65ef04656eb763bfce4f0bfc773d9a956221c
Emre Savaş <emre_savas28@hotmail.com>
2023-11-20 16:41:21 +0300

    Create CreateMedicationDto and add mapping.

---
+++ DoctorAppointmentsAPI/AutoMapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DoctorAppointmentsAPI.DataTransferObjects;
+using DoctorAppointmentsDomain.Entities;
+
+namespace DoctorAppointmentsAPI.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateMedicationDto, AppointmentMedications>();
+        }
+    }
+}
