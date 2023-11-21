be66ffb2967a867acaf4812671aeffbd825a70a8
Emre Savaş <emre_savas28@hotmail.com>
2023-11-21 17:51:30 +0300

    Create some other end points in DoctorController.

--- DoctorAppointmentsAPI/Controllers/DoctorController.cs
+++ DoctorAppointmentsAPI/Controllers/DoctorController.cs
@@ -1,4 +1,5 @@
-﻿using DoctorAppointmentsAPI.Services.Contracts;
+﻿using DoctorAppointmentsAPI.DataTransferObjects;
+using DoctorAppointmentsAPI.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DoctorAppointmentsAPI.Controllers
@@ -18,7 +19,35 @@ namespace DoctorAppointmentsAPI.Controllers
         public async Task<IActionResult> GetPatientsAsync([FromRoute(Name = "doctorId")] int doctorId)
         {
             var patients = await _serviceManager.Doctor.GetPatientsAsync(doctorId, false);
+            if (patients == null)
+                return NotFound("You don't have any patient.");
             return Ok(patients);
         }
+
+        [HttpGet("TodaysAppointments/{doctorID:int}", Name = "GetTodaysAppointmentsAsync")]
+        public async Task<IActionResult> GetTodaysAppointmentsAsync([FromRoute(Name = "doctorId")] int doctorId)
+        {
+            var appointments = await _serviceManager.Doctor.GetTodaysAppointmentsAsync(doctorId, false);
+            if (appointments == null)
+                return NotFound("You don't have any appointment today.");
+            return Ok(appointments);
+        }
+
+        [HttpGet("UpcomingAppointments/{doctorID:int}", Name = "GetUpcomingAppointmentsAsync")]
+        public async Task<IActionResult> GetUpcomingAppointmentsAsync([FromRoute(Name = "doctorId")] int doctorId)
+        {
+            var appointments = await _serviceManager.Doctor.GetUpcomingAppointmentsAsync(doctorId, false);
+            if (appointments == null)
+                return NotFound("You don't have any upcoming appointment.");
+            return Ok(appointments);
+        }
+
+        [HttpPost("WriteMedication/{doctorID:int}", Name = "WriteMedicationAsync")]
+        public async Task<IActionResult> WriteMedicationAsync([FromRoute(Name = "doctorId")] int doctorId, [FromBody] CreateMedicationDto medicationDto)
+        {
+            var medicationId = await _serviceManager.Doctor.CreateMedicationAsync(doctorId, medicationDto, false);
+            return CreatedAtRoute("GetMedicationAsync", new { medicationId }, medicationDto);
+        }
+
     }
 }
