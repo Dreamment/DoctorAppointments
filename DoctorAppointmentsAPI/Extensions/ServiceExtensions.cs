cb38348845b8b1783b9ed7488d8f23024b981898
Emre Savaş <emre_savas28@hotmail.com>
2023-11-27 22:53:05 +0300

    Extract service configuration to ServiceExtensions.

---
+++ DoctorAppointmentsAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+using Services.Contracts;
+using Services;
+
+namespace DoctorAppointmentsAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+            => services.AddDbContext<RepositoryContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IDoctorService, DoctorManager>();
+            services.AddScoped<IPatientService, PatientManager>();
+            services.AddScoped<IAdminService, AdminManager>();
+        }
+    }
+}
--- DoctorAppointmentsAPI/Program.cs
+++ DoctorAppointmentsAPI/Program.cs
@@ -1,8 +1,4 @@
-using Repositories;
-using Repositories.Contracts;
-using Services;
-using Services.Contracts;
-using Microsoft.EntityFrameworkCore;
+using DoctorAppointmentsAPI.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,12 +11,8 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<RepositoryContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
-builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
-builder.Services.AddScoped<IDoctorService, DoctorManager>();
-builder.Services.AddScoped<IPatientService, PatientManager>();
-builder.Services.AddScoped<IAdminService, AdminManager>();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureCustomServices();
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
